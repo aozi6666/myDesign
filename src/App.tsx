431038d9dc8e109753173f3691f9ca66fc345976
zhangao <2212433186@qq.com>
2026-03-09 18:50:05 +0800

    3.9

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{ marginBottom: 24, fontWeight: 500 }}>Button 组件演示</h1>
+        <h2 style={{ marginBottom: 24, fontWeight: 500 }}>Button 组件演示</h2>
         <div
           style={{
             display: 'flex',
@@ -22,14 +22,13 @@ const App: FC = () => {
           }}
         >
           <Button btnType={ButtonType.Primary}>Primary Button</Button>
-          
           <Button btnType={ButtonType.Default}>Default Button</Button>
           <Button btnType={ButtonType.Danger}>Danger Button</Button>
-          <Button btnType={ButtonType.Link} href="#">
-            Link Button
+          <Button btnType={ButtonType.Link} href="https://www.baidu.com/">
+            Link 百度
           </Button>
-          <Button btnType={ButtonType.Link} href="#" target="_blank">
-            Link Button("新窗口打开")
+          <Button btnType={ButtonType.Link} href="https://www.baidu.com/" target="_blank">
+            Link 百度("新窗口打开")
           </Button>
         </div>
       </header>
