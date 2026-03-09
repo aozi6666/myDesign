5ce3fa9074d51e83b81accdce3bc747e0ef7b804
zhangao <2212433186@qq.com>
2026-03-09 16:53:24 +0800

    3.9

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,4 +1,19 @@
+/*
+React 函数组件调用:
+  - 生成 class ：浏览器 “用 class 去匹配 CSS选择器” 
+  - 生成元素 : return <button/> / return <a/>
+导出一个 React 组件函数
+
+ 外部使用<Button>...</Button>
+  -> React 会执行 Button(props) 
+  -> 组件 return JSX 
+  -> React 把 JSX 变成真实 DOM
+  -> 浏览器显示 HTML
+*/
+
+// FC 是 React 提供的一个 组件类型（Function Component 函数组件）
 import type { FC } from 'react'
+// 第三方工具库： 自动智能拼接 class 字符串
 import classNames from 'classnames'
 import type {
   AnchorButtonProps,
@@ -23,9 +38,20 @@ const Button: FC<ButtonProps> = ({
   用户写：<Button btnType="primary" size="lg">
   组件算出 class：btn btn-primary btn-lg
   */
+
+  // 返回 字符串
   const classes = classNames('btn', className, {
+    /*
+       外部传来 <Button btnType={ButtonType.Primary}>
+         -> 变成 { "btn-primary": true }
+         -> classNames 会加上: btn-primary
+         -> 最终变为 ： btn btn-primary 
+    */
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
+
+    // 是 link 按钮 &&  disabled === true
+    // classNames 会加上: disabled
     disabled: btnType === ButtonType.Link && disabled,
   })
 
@@ -60,4 +86,5 @@ const Button: FC<ButtonProps> = ({
   )
 }
 
+// 导出一个 React 组件函数
 export default Button
--- src/components/Button/button.types.ts
+++ src/components/Button/button.types.ts
@@ -1,15 +1,25 @@
-/* 组件 API： 规定 Button 可以怎么被使用 */
+/* 定义 Button 组件可以接收哪些 props（组件 API） */
+
+
+// 导入了 3 个 React 内置类型
 import type {
   AnchorHTMLAttributes,
   ButtonHTMLAttributes,
   ReactNode,
 } from 'react'
 
+// TS 导出 两个固定值 'lg' / 'sm'
+/*
+as const: 把 ButtonSize对象的值 变成不可修改的 字面量类型
+  - 如果没有 as const: Large: string
+  - 如果有 as const: Large: "lg"
+*/
 export const ButtonSize = {
   Large: 'lg',
   Small: 'sm',
 } as const
 
+// 最终生成的类型: type ButtonSize = "lg" | "sm"
 export type ButtonSize =
   (typeof ButtonSize)[keyof typeof ButtonSize]
 
@@ -20,10 +30,12 @@ export const ButtonType = {
   Link: 'link',
 } as const
 
+// 最终生成的类型: type ButtonType = "primary" | "default" | "danger" | "link"
 export type ButtonType =
   (typeof ButtonType)[keyof typeof ButtonType]
 
-  // Button 的 props
+  // 定义了 Button 支持的 属性Props
+  // className: 自定义的 class
 export interface BaseButtonProps {
   className?: string
   disabled?: boolean
@@ -33,12 +45,19 @@ export interface BaseButtonProps {
   href?: string
 }
 
+/*
+ &：合并两个类型   |: 交叉
+ NativeButtonProps = BaseButtonProps + button 所有原生属性（onClick={handleClick}）
+*/
 export type NativeButtonProps = BaseButtonProps &
   ButtonHTMLAttributes<HTMLButtonElement>
 
+// Button 渲染成 <a>
 export type AnchorButtonProps = BaseButtonProps &
   AnchorHTMLAttributes<HTMLAnchorElement>
 
+// ButtonProps（最终类型）：NativeButtonProps & AnchorButtonProps 
+// TypeScript 内置工具类型Partial<>：把所有属性变成可选
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
 
 
