fd6c9223be2f8adc98f85f62e561c3f169a30a05
zhangao <2212433186@qq.com>
2026-03-13 08:26:12 +0800

    3.13

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -1,61 +1,77 @@
-import React  from 'react'
-import type { ComponentMeta } from '@storybook/react'
+import React from 'react'
+import type { Meta, StoryFn } from '@storybook/react-vite'
 import { Upload } from './upload'
 import Button from '../Button/button'
 import Icon from '../Icon/icon'
 
-export default { 
-  title: '第十章：Upload',
+// Meta = storybook 组件说明的类型
+// typeof Upload：Upload 这个组件函数的类型
+// Meta<typeof Upload>：Upload 组件的 storybook meta 类型
+const meta = {
+  title: 'Upload（文件选择上传+拖拽）',
   id: 'Upload',
   component: Upload,
   parameters: {
     docs: {
       source: {
-        type: "code",
+        type: 'code',
       },
-    }
-  }
-} as ComponentMeta<typeof Upload>
+    },
+  },
+} satisfies Meta<typeof Upload>
 
-export const ASimpleUpload = (args) => (
-  <Upload
-    {...args}
-    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-  >
-    <Button size="lg" btnType="primary"><Icon icon="upload" /> 点击上传 </Button>
-  </Upload>  
-)
+export default meta
+
+// 推荐写法：用 Template 复用渲染逻辑，再用 bind({}) 生成具体 story
+const Template: StoryFn<typeof Upload> = (args) => <Upload {...args} />
+
+// 1. 普通上传
+export const ASimpleUpload = Template.bind({})
+ASimpleUpload.args = {
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  children: (
+    <Button size="lg" btnType="primary">
+      <Icon icon="upload" />
+      点击上传
+    </Button>
+  ),
+}
 ASimpleUpload.storyName = '普通的 Upload 组件'
-export const BCheckUpload = (args) => {
-  const checkFileSize = (file: File) => {
-    if (Math.round(file.size / 1024) > 50) {
-      alert('file too big')
-      return false;
-    }
-    return true;
+
+// 2. 上传前检查文件大小
+const checkFileSize = (file: File) => {
+  if (Math.round(file.size / 1024) > 50) {
+    alert('file too big')
+    return false
   }
-  return (
-    <Upload
-      {...args}
-      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-      beforeUpload={checkFileSize}
-    >
-      <Button size="lg" btnType="primary"><Icon icon="upload" /> 不能传大于50Kb！ </Button>
-    </Upload>  
-  )
+  return true
+}
+
+export const BCheckUpload = Template.bind({})
+BCheckUpload.args = {
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  beforeUpload: checkFileSize,
+  children: (
+    <Button size="lg" btnType="primary">
+      <Icon icon="upload" /> 不能传大于50Kb！
+    </Button>
+  ),
 }
 BCheckUpload.storyName = '上传前检查文件大小'
-export const CDragUpload = (args) => (
-  <Upload
-    {...args}
-    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-    name="fileName"
-    multiple
-    drag
-  >
-    <Icon icon="upload" size="5x" theme="secondary" />
-    <br/>
-    <p>点击或者拖动到此区域进行上传</p>
-  </Upload>
-)
+
+// 3. 拖拽上传
+export const CDragUpload = Template.bind({})
+CDragUpload.args = {
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  name: 'fileName',
+  multiple: true,
+  drag: true,
+  children: (
+    <>
+      <Icon icon="upload" size="5x" theme="secondary" />
+      <br />
+      <p>点击或者拖动到此区域进行上传</p>
+    </>
+  ),
+}
 CDragUpload.storyName = '拖动上传'
