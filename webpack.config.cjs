645acdf30649e4dfff9e4eea0ffa4c52156d0aef
zhangao <2212433186@qq.com>
2026-03-14 11:51:19 +0800

    3.14

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: isProd ? 'production' : 'development',
   entry: './Webpack/main.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -10,13 +14,37 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
   },
+  // 配置loader：文件转换器
   module: {
     rules: [
       {
+        // 匹配文件：以 .ts 结尾的文件
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      // 匹配文件：以 .css 结尾的文件
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
     ],
   },
+  // 插件
+  plugins: [
+    // 自动生成index.html文件
+    new HtmlWebpackPlugin({
+      template: './Webpack/index.html',
+    }),
+  ],
+  // 仅生产环境压缩
+  optimization: {
+    minimize: isProd,
+    ...(isProd && { minimizer: [new TerserPlugin()] }),
+  },
+  // 配置服务器
+  devServer: {
+    port: 3000,
+    open: true
+  }
 };
